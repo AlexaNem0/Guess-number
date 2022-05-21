40c4a7d1033c950fc455d4e80f059a26ca28e331
“AlexaNem0” <vikalyeksyeyev@gmail.com>
2022-05-21 15:33:46 -0500

    Guess-number

--- script.js
+++ script.js
@@ -78,5 +78,3 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
 });
-
-//GitHub comitt
